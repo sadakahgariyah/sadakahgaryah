d5cbf469837b498847553757d553e176cfecbf6f
sadakahgariyah <ahmedradad1324@gmail.com>
2026-03-22 18:03:50 +0200

    Initial commit

---
+++ my_app/lib/pages/Home-page/home_page_widget.dart
@@ -0,0 +1,35 @@
+import '/backend/schema/structs/index.dart';
+import '/flutter_flow/flutter_flow_button_tabbar.dart';
+import '/flutter_flow/flutter_flow_data_table.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
+import 'home_page_widget.dart' show HomePageWidget;
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class HomePageModel extends FlutterFlowModel<HomePageWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  // State field(s) for TabBar widget.
+  TabController? tabBarController;
+  int get tabBarCurrentIndex =>
+      tabBarController != null ? tabBarController!.index : 0;
+  int get tabBarPreviousIndex =>
+      tabBarController != null ? tabBarController!.previousIndex : 0;
+
+  // State field(s) for PaginatedDataTable widget.
+  final paginatedDataTableController =
+      FlutterFlowDataTableController<dynamic>();
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {
+    tabBarController?.dispose();
+    paginatedDataTableController.dispose();
+  }
+}
---
+++ my_app/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\programming\\Flutter\\flutter_windows_3.35.1-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Tech Shop\\OneDrive\\Desktop\\New folder\\my_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\programming\\Flutter\\flutter_windows_3.35.1-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Tech Shop\\OneDrive\\Desktop\\New folder\\my_app"
+  "FLUTTER_ROOT=D:\\programming\\Flutter\\flutter_windows_3.35.1-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Tech Shop\\OneDrive\\Desktop\\New folder\\my_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Tech Shop\\OneDrive\\Desktop\\New folder\\my_app"
+  "FLUTTER_TARGET=C:\\Users\\Tech Shop\\OneDrive\\Desktop\\New folder\\my_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzUuMQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049MjBmODI3NDkzOQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MWU5YTgxMWJmOA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjA="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Tech Shop\\OneDrive\\Desktop\\New folder\\my_app\\.dart_tool\\package_config.json"
+)
